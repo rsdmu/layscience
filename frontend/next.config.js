e0a1fc43d31aebd031b21f0faa9c30882f011b5c
Rashid Mushkani <113534995+rsdmu@users.noreply.github.com>
2025-08-24 11:09:37 -0400

    Add API rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true }
+  experimental: { typedRoutes: true },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://layscience.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
